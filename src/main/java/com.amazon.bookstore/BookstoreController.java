101c852a44035815b664985ca264b5a44576dea4
mounicap6 <mounica.pillarisetty@gmail.com>
2021-03-09 18:58:10 -0500

    simple html display

---
+++ src/main/java/com.amazon.bookstore/BookstoreController.java
@@ -0,0 +1,23 @@
+package com.amazon.bookstore;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class BookstoreController {
+
+    @GetMapping("/")
+    public String index(Model model) {
+        Book b1 = new Book("Harry Potter", "JK Rowling", "N/A", 123, "HP", "Fantasy");
+        Book b2 = new Book("Lord of the Rings", "JRR Tolkien", "N/A", 124, "LOTR", "Fantasy");
+        BookStore books = new BookStore();
+        books.addBook(b1);
+        books.addBook(b2);
+
+        model.addAttribute("bookstore", books);
+
+        return "Bookstore";
+    }
+
+}
