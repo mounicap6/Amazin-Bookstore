ad1e09f0adbae3f41cd1a3ec4d720ae877096f9e
SoniaHL <36013810+SoniaHL@users.noreply.github.com>
2021-03-08 20:51:56 -0500

    Defined and initialized variables and created constructors

--- src/main/java/com.amazon.bookstore/Book.java
+++ src/main/java/com.amazon.bookstore/Book.java
@@ -1,4 +1,75 @@
 package com.amazon.bookstore;
 
+import java.awt.image.BufferedImage;
+
 public class Book {
+
+    private int isbn;
+    private String title, description, author, publisher, genre;
+    private BufferedImage image;
+
+    public Book(String title, String author, String publisher, int isbn, String description, String genre){
+        this.title = title;
+        this.author = author;
+        this.publisher = publisher;
+        this.isbn = isbn;
+        this.description = description;
+        this.genre = genre;
+    }
+
+    public int getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(int isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public BufferedImage getImage() {
+        return image;
+    }
+
+    public void setImage(BufferedImage image) {
+        this.image = image;
+    }
 }
--- src/main/java/com.amazon.bookstore/BookStore.java
+++ src/main/java/com.amazon.bookstore/BookStore.java
@@ -1,4 +1,20 @@
 package com.amazon.bookstore;
 
+import java.util.ArrayList;
+
 public class BookStore {
+
+    ArrayList<Book> books;
+
+    public BookStore(){
+        books = new ArrayList<>();
+    }
+
+    public boolean addBook(Book b){
+        return books.add(b);
+    }
+
+    public boolean removeBook(Book b){
+        return books.remove(b);
+    }
 }
