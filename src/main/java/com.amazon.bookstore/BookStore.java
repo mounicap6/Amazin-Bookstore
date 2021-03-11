fca1dbcf7edeada558cc14ef8c301686c1fef549
SoniaHL <36013810+SoniaHL@users.noreply.github.com>
2021-03-11 15:58:24 -0500

    Moved annotations to variable declarations instead of getters

--- src/main/java/com.amazon.bookstore/BookStore.java
+++ src/main/java/com.amazon.bookstore/BookStore.java
@@ -7,10 +7,13 @@ import javax.persistence.*;
 @Entity
 public class BookStore {
 
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
 
     private String name;
 
+    @OneToMany(cascade = CascadeType.ALL)
     List<Book> books;
 
     public BookStore(){
@@ -21,18 +24,17 @@ public class BookStore {
         this.books = new ArrayList<Book>();
     }
 
-    @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
+
     public Long getId() { return this.id; }
     public void setId(Long id) { this.id = id; }
 
-    @OneToMany(cascade = CascadeType.ALL)
+
     public List<Book> getBooks(){ return this.books; }
     public void setBooks(List<Book> books){ this.books = books; }
 
 
     public boolean addBook(Book b){
-        b.setBookstore(this);
+        //b.setBookstore(this);
         return books.add(b);
     }
 
