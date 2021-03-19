61a973413c7fadf1551b790bcaefa5d49816c38c
mounicap6 <mounica.pillarisetty@gmail.com>
2021-03-19 12:10:58 -0400

    able to upload books

--- src/main/java/com.amazon.bookstore/BookstoreController.java
+++ src/main/java/com.amazon.bookstore/BookstoreController.java
@@ -14,12 +14,10 @@ public class BookstoreController {
 
     @GetMapping("/")
     public String index(Model model) {
-        Book b1 = new Book("Harry Potter", "JK Rowling", "N/A", 123, "HP", "Fantasy");
-        Book b2 = new Book("Lord of the Rings", "JRR Tolkien", "N/A", 124, "LOTR", "Fantasy");
-        BookStore books = new BookStore();
-        books.addBook(b1);
-        books.addBook(b2);
-
+       BookStore books = new BookStore();
+        for(Book b : bookRepo.findAll()){
+            books.addBook(b);
+        }
         model.addAttribute("bookstore", books);
 
         return "bookstore";
@@ -32,7 +30,8 @@ public class BookstoreController {
     }
 
     @PostMapping("/uploadBook")
-    public Book createBook(@RequestBody Book book){
+    @ResponseBody
+    public Book createBook(Book book){
         Book b = bookRepo.save(book);
         return b;
     }
