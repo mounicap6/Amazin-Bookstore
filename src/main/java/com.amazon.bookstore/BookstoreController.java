051ecdf31a599d4a8be01bb66c24811827d116b7
mounicap6 <mounica.pillarisetty@gmail.com>
2021-03-22 11:58:07 -0400

    index modification

--- src/main/java/com.amazon.bookstore/BookstoreController.java
+++ src/main/java/com.amazon.bookstore/BookstoreController.java
@@ -14,18 +14,6 @@ public class BookstoreController {
 
     @GetMapping("/")
     public String index(Model model) {
-        Book b1 = new Book("Harry Potter", "JK Rowling", "N/A", 123, "HP", "Fantasy", 1);
-        Book b2 = new Book("Lord of the Rings", "JRR Tolkien", "N/A", 124, "LOTR", "Fantasy", 2);
-        BookStore books = new BookStore();
-        books.addBook(b1);
-        books.addBook(b2);
-
-        model.addAttribute("bookstore", books);
-
-        return "bookstore";
-    }
-    @GetMapping("/")
-    public String index(Model model) {
         BookStore books = new BookStore();
         for(Book b : bookRepo.findAll()){
             books.addBook(b);
@@ -37,15 +25,14 @@ public class BookstoreController {
 
     @PostMapping("/editBookstore")
     public String uploadBookstore(){
-
+        //functionality here
         return "editBookstore";
     }
 
     @PostMapping("/uploadBook")
-    @ResponseBody
-    public Book createBook(Book book){
-        Book b = bookRepo.save(book);
-        return b;
+    public String createBook(Book book){
+        bookRepo.save(book);
+        return "bookstore";
     }
 
 }
