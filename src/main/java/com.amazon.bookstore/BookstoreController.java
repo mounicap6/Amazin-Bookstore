4f1b7abe57d330332b957387261964aa0274596f
lynn <lynnmehyou@hotmail.com>
2021-04-05 09:00:07 -0400

    edited

--- src/main/java/com.amazon.bookstore/BookstoreController.java
+++ src/main/java/com.amazon.bookstore/BookstoreController.java
@@ -1,11 +1,14 @@
 package com.amazon.bookstore;
 
 import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 
@@ -103,5 +106,12 @@ public class BookstoreController {
         return "userBookstore";
     }
 
+
+
+    @RequestMapping(value= "/delete/{id}", method = RequestMethod.GET)
+    public String deleteBook(@PathVariable("id")  long bookId, Model model){
+       bookRepo.deleteById(bookId);
+       return "redirect:../ownerBookstore";
+    }
 
 }
