9c6a857115130d1a8612cb6284130eb3fd483e72
SoniaHL <36013810+SoniaHL@users.noreply.github.com>
2021-04-09 11:01:22 -0400

    Initial websocket setup (messaging on homepage)

---
+++ src/main/java/com.amazon.bookstore/Message.java
@@ -0,0 +1,27 @@
+package com.amazon.bookstore;
+
+public class Message {
+    private String from;
+    private String text;
+
+    public Message(String from, String text, String date){
+        this.from = from;
+        this.text = text;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
---
+++ src/main/java/com.amazon.bookstore/MessageHandlingController.java
@@ -0,0 +1,19 @@
+package com.amazon.bookstore;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Controller
+public class MessageHandlingController {
+    @MessageMapping("/chat")
+    @SendTo("/topic/messages")
+    public OutputMessage send(Message message) throws Exception {
+        String time = new SimpleDateFormat("HH:mm").format(new Date());
+        return new OutputMessage(message.getFrom(), message.getText(), time);
+    }
+}
+
