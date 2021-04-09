a3f464c41a2538e8b8c9fcd9be53c16e5040c9b8
mounicap6 <mounica.pillarisetty@gmail.com>
2021-04-08 23:50:09 -0400

    tests testGetDistance, testFindClosestUser, testGetRecommendations and testGetFurthestUser

--- src/test/java/com/amazon/bookstore/JaccardDistanceTest.java
+++ src/test/java/com/amazon/bookstore/JaccardDistanceTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.junit.jupiter.api.Test;
 
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -14,10 +15,9 @@ public class JaccardDistanceTest {
 
     @Autowired
     private JaccardDistance jaccardDistance;
-
     private User user1, user2, user3;
     private Book book1, book2, book3, book4;
-    HashMap<User,Double> distanceBySimiliarUserMap = new HashMap<User,Double>();
+    private ArrayList<User> users;
 
     @BeforeEach
     public void setUp(){
@@ -26,6 +26,8 @@ public class JaccardDistanceTest {
         this.user1 = new User("user1");
         this.user2 = new User("user2");
         this.user3 = new User("user3");
+
+
 
         this.book1 = new Book("Book1", "Paulo Coelho", "HarperCollins", 97800613, "pursue your dreams by following what your heart desires", "Adventure",3,"https://upload.wikimedia.org/wikipedia/en/c/c4/TheAlchemist.jpg");
         this.book2 = new Book("Book2", "Paulo Coelho", "HarperCollins", 34534534, "des", "Adventure",3,"https://upload.wikimedia.org/wikipedia/en/c/c4/TheAlchemist.jpg");
@@ -39,6 +41,10 @@ public class JaccardDistanceTest {
         this.user3.addToCart(this.book2);
         this.user3.addToCart(this.book3);
         this.user3.addToCart(this.book4);
+
+        users = new ArrayList<>();
+        users.add(this.user1);
+        users.add(this.user3);
     }
 
     @Test
@@ -54,17 +60,28 @@ public class JaccardDistanceTest {
 
     @Test
     public void testFindClosestUser(){
+        HashSet<Book> expectedRecommendations = new HashSet<>();
+        expectedRecommendations.add(this.book3);
+        expectedRecommendations.add(this.book2);
 
+        assertEquals(users.size(),2);
+        assertEquals(expectedRecommendations.size(),2);
     }
 
     @Test
     public void testGetDistance(){
         assertEquals(jaccardDistance.getDistance(this.user1, this.user1),100.00);
+        assertEquals(jaccardDistance.getDistance(this.user1, this.user2),100.00);
     }
 
     @Test
     public void testGetRecommendations(){
+        HashSet<Book> expectedRecommendations = new HashSet<Book>();
+        expectedRecommendations.add(this.book3);
+        expectedRecommendations.add(this.book1);
+        expectedRecommendations.add(this.book2);
 
+        assertEquals(expectedRecommendations.size(), 3);
     }
 
 }
